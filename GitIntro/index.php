008e8cefb272b712f6ea30b2defe870b3593bd54
ChadHacksaLot <150989535+ChadHacksaLot@users.noreply.github.com>
2024-01-23 21:49:33 -0500

    hello world. Soon to get the nix

---
+++ GitIntro/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+
+<html>
+        <head>
+            <meta charset="utf-8">
+            <title>title</title>
+        </head>
+        <body>
+            <?php
+            echo "Hello World";
+            ?>
+
+        </body>
+    </html>
